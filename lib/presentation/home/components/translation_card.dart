7c90d8cc4e6c9ef7d28cf006086bb24b3d0063e2
CoderUni <57564335+CoderUni@users.noreply.github.com>
2025-05-20 02:37:34 +0800

    feat: open translated text in pleco

--- lib/presentation/home/components/translation_card.dart
+++ lib/presentation/home/components/translation_card.dart
@@ -4,6 +4,7 @@ import 'package:gap/gap.dart';
 import 'package:qack/layout/layout_handler.dart';
 import 'package:qack/presentation/home/models/base_translation_details.dart';
 import 'package:qack/theme/themes/light_theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
 class TranslationCard extends StatelessWidget {
@@ -14,13 +15,14 @@ class TranslationCard extends StatelessWidget {
   Widget build(BuildContext context) {
     const theme = LightTheme();
     return LayoutHandler(
-      mobile: MobileTranslationCard(
+      mobile: TranslationCardView(
         translationDetails: translationDetails,
         cardMargin: const EdgeInsets.only(bottom: 16),
         cardPadding:
-            const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 12),
+            const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 4),
         titleGap: 8,
-        translationSpacing: 8,
+        translationMargin: const EdgeInsets.symmetric(vertical: 8),
+        translationPadding: const EdgeInsets.all(8),
         titleStyle: Theme.of(context).textTheme.titleMedium!.copyWith(
               fontWeight: FontWeight.w600,
               color: theme.textColor2,
@@ -29,13 +31,14 @@ class TranslationCard extends StatelessWidget {
               color: theme.textColor1,
             ),
       ),
-      tablet: TabletTranslationCard(
+      tablet: TranslationCardView(
         translationDetails: translationDetails,
         cardMargin: const EdgeInsets.only(bottom: 16),
         cardPadding:
-            const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 12),
+            const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 4),
         titleGap: 8,
-        translationSpacing: 8,
+        translationMargin: const EdgeInsets.symmetric(vertical: 8),
+        translationPadding: const EdgeInsets.all(8),
         titleStyle: Theme.of(context).textTheme.titleMedium!.copyWith(
               fontWeight: FontWeight.w600,
               color: theme.textColor2,
@@ -48,13 +51,14 @@ class TranslationCard extends StatelessWidget {
   }
 }
 
-class MobileTranslationCard extends StatelessWidget {
-  const MobileTranslationCard({
+class TranslationCardView extends StatelessWidget {
+  const TranslationCardView({
     required this.translationDetails,
     required this.cardMargin,
     required this.cardPadding,
     required this.titleGap,
-    required this.translationSpacing,
+    required this.translationMargin,
+    required this.translationPadding,
     required this.titleStyle,
     required this.translatedTextStyle,
     super.key,
@@ -65,7 +69,9 @@ class MobileTranslationCard extends StatelessWidget {
   final EdgeInsets cardPadding;
 
   final double titleGap;
-  final double translationSpacing;
+
+  final EdgeInsets translationMargin;
+  final EdgeInsets translationPadding;
 
   final TextStyle titleStyle;
   final TextStyle translatedTextStyle;
@@ -105,80 +111,29 @@ class MobileTranslationCard extends StatelessWidget {
                   const Spacer(),
                 ],
               ),
-              Gap(translationSpacing),
-              Text(
-                translationDetails.translatedText.outputText,
-                style: translatedTextStyle,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TabletTranslationCard extends StatelessWidget {
-  const TabletTranslationCard({
-    required this.translationDetails,
-    required this.cardMargin,
-    required this.cardPadding,
-    required this.titleGap,
-    required this.translationSpacing,
-    required this.titleStyle,
-    required this.translatedTextStyle,
-    super.key,
-  });
-  final BaseTranslationDetails translationDetails;
-
-  final EdgeInsets cardMargin;
-  final EdgeInsets cardPadding;
-
-  final double titleGap;
-  final double translationSpacing;
-
-  final TextStyle titleStyle;
-  final TextStyle translatedTextStyle;
-
-  @override
-  Widget build(BuildContext context) {
-    const theme = LightTheme();
-    return Padding(
-      padding: cardMargin,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: theme.cardColor,
-          boxShadow: theme.cardShadow,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Padding(
-          padding: cardPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    height: 28,
-                    width: 28,
-                    child: SvgPicture(
-                      AssetBytesLoader(
-                        translationDetails.svgPath,
+              Padding(
+                padding: translationMargin,
+                child: InkWell(
+                  onTap: () {
+                    launchUrl(
+                      Uri(
+                        scheme: 'plecoapi',
+                        host: 'x-callback-url',
+                        path: 's',
+                        queryParameters: {
+                          'q': translationDetails.translatedText.outputText,
+                        },
                       ),
+                    );
+                  },
+                  child: Padding(
+                    padding: translationPadding,
+                    child: Text(
+                      translationDetails.translatedText.outputText,
+                      style: translatedTextStyle,
                     ),
                   ),
-                  Gap(titleGap),
-                  Text(
-                    translationDetails.translatorName,
-                    style: titleStyle,
-                  ),
-                  const Spacer(),
-                ],
-              ),
-              Gap(translationSpacing),
-              Text(
-                translationDetails.translatedText.outputText,
-                style: translatedTextStyle,
+                ),
               ),
             ],
           ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import package_info_plus
 import path_provider_foundation
 import sentry_flutter
 import speech_to_text
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
